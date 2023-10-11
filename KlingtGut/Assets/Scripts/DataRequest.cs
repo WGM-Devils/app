b719a732bec2e4e3691e351d14a32ff767256419
Philipp.Thieben <philippthieben77@gmail.com>
2023-10-11 14:02:35 +0200

    3

---
+++ KlingtGut/Assets/Scripts/DataRequest.cs
@@ -0,0 +1,25 @@
+//using Newtonsoft.Json;
+using TMPro;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DataRequest : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hi");
+    }
+
+
+    //IEnumerator GetRequest(string uri)
+    //{
+    //    using (UnityWebRequest)
+    //    {
+
+    //    }
+    //}
+    
+}
