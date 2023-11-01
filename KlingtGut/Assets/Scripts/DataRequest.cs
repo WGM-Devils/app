0ee8f34b65471ef656fa1ca3b006a5f8faa77507
Philipp.Thieben <philippthieben77@gmail.com>
2023-11-01 15:11:09 +0100

    7
    messages working

--- KlingtGut/Assets/Scripts/DataRequest.cs
+++ KlingtGut/Assets/Scripts/DataRequest.cs
@@ -5,21 +5,61 @@ using UnityEngine.Networking;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+[System.Serializable]
+public class implementation
+{
+    public string type;
+    public string content;
+}
+[System.Serializable]
+public class innerData
+{
+    public int user;
+    public string title;
+    public string description;
+    public implementation imp;
+    public string createdAt;
+    public string updatedAt;
+}
+[System.Serializable]
+public class Messages
+{
+    public innerData[] Message;
+}
 public class DataRequest : MonoBehaviour
 {
+    public TMP_InputField output;
+    public string response;
+    public TextAsset responseText;
+
     void Start()
     {
-        Debug.Log("Hi");
+       
+        StartCoroutine(GetRequest());
     }
 
 
-    //IEnumerator GetRequest(string uri)
-    //{
-    //    using (UnityWebRequest)
-    //    {
+    IEnumerator GetRequest()
+    {
+        string uri = "https://official.klingt-gut.repl.co/api/posts/all";
+        using (UnityWebRequest request = UnityWebRequest.Get(uri))
+        {
+            request.SetRequestHeader("Content-Type", "application/json");
+            request.SetRequestHeader("Authorization", "KlingtGut");
+            yield return request.SendWebRequest();
 
-    //    }
-    //}
-    
+
+            if (request.isNetworkError || request.isHttpError)
+            {
+                output.text = "error";
+
+            }
+            else
+            {
+                Messages myData = JsonUtility.FromJson<Messages>(request.downloadHandler.text);
+                Debug.Log(myData.Message[0].user);
+            }
+        }
+    }
+
 }
