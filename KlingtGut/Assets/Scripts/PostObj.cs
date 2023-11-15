7151548011be960f418d2ef74b10eca7f30a402a
Philipp.Thieben <philippthieben77@gmail.com>
2023-11-15 15:14:48 +0100

    10
    messages

---
+++ KlingtGut/Assets/Scripts/PostObj.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PostObj : MonoBehaviour
+{
+
+
+
+    public GameObject Posts;
+    public messages Message;
+    public int Id;
+    public Text userId;
+    public Text Title;
+    public Text description;
+    public Text commentsCount;
+    public Text likesCount;
+    public Text viewsCount;
+    public Text createdAt;
+
+
+
+    public void Start()
+    {
+        Message = Posts.GetComponent<PostRequest>().Data.messages[Id];
+
+        Title.text = Message.title;
+    }
+}
---
+++ KlingtGut/Assets/Scripts/PostRequest.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using TMPro;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public class views
+    {
+        int count;
+        int[] id;
+    }
+    public class likes
+    {
+        int count;
+        int[] collection;
+    }
+
+    public class comments
+    {
+        bool allowed;
+        public int count;
+        public int[] Id;
+    }
+
+    [System.Serializable]
+    public class embed
+    {
+        public string type { get; set; }
+        public string link { get; set; }
+    }
+
+    [System.Serializable]
+    public class messages
+    {
+        public int Id;
+        public string user;
+        public string title;
+        public string description;
+        public embed implementation;
+        public comments comment;
+        public likes like;
+        public views wiews;
+        public string createdAt;
+        public string lastUpdated;
+    }
+    [System.Serializable]
+
+    public class Root
+    {
+        public messages[] messages;
+
+    }
+
+public class PostRequest : MonoBehaviour
+{ 
+
+
+
+
+    public Root Data = new Root();
+
+
+
+
+
+    public TMP_InputField output;
+
+
+
+
+    public void Start()
+    {
+        StartCoroutine(GetRequest());
+    }
+
+
+    IEnumerator GetRequest()
+    {
+        string uri = "https://official.klingt-gut.repl.co/api/posts/all";
+        using (UnityWebRequest request = UnityWebRequest.Get(uri))
+        {
+            request.SetRequestHeader("Content-Type", "application/json");
+            request.SetRequestHeader("Authorization", "KlingtGut");
+            yield return request.SendWebRequest();
+
+
+            if (request.isNetworkError || request.isHttpError)
+            {
+                //output.text = "error";
+
+            }
+            else
+            {
+                //output.text = request.downloadHandler.text;
+                Data = JsonConvert.DeserializeObject<Root>(request.downloadHandler.text);
+
+
+                Debug.Log(Data.messages[1].wiews);
+
+
+            }
+        }
+    }
+
+}
