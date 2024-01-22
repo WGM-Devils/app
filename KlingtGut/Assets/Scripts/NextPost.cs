b53fc76f9a0eaf5c4470e4b7afbac8e6c6b10e5c
Philipp.Thieben <philippthieben77@gmail.com>
2024-01-22 22:18:43 +0100

    19

---
+++ KlingtGut/Assets/Scripts/NextPost.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextPost : MonoBehaviour
+{
+    private SwipePost swipe;
+
+    private GameObject prePost;
+    void Start()
+    {
+        swipe = FindObjectOfType<SwipePost>();
+        prePost = swipe.gameObject;
+
+        transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceMoved = prePost.transform.localPosition.x;
+        if (Mathf.Abs(distanceMoved)> 0)
+        {
+            float step = Mathf.SmoothStep(0.8f, 1, Mathf.Abs(distanceMoved) / (Screen.width / 2));
+            transform.localScale = new Vector3(step, step, step);
+        }
+        
+    }
+}
---
+++ KlingtGut/Assets/Scripts/SwipePost.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SwipePost : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    private Vector3 _initialPosition;
+    private float _distanceMoved;
+    private bool _swipeLeft;
+    public RectTransform Pos;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.localPosition = new Vector2(transform.localPosition.x + eventData.delta.x, transform.localPosition.y);
+        
+        if(Pos.localPosition.x - _initialPosition.x > 0 )
+        {
+            transform.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(0, -30, (_initialPosition.x + transform.localPosition.x)/(Screen.width/2)));
+        }
+        else
+        {
+            transform.localEulerAngles = new Vector3(0,0,Mathf.Lerp(0,30,(_initialPosition.x - transform.localPosition.x)/(Screen.width/2)));
+        }
+
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _initialPosition = transform.localPosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _distanceMoved = Mathf.Abs(transform.localPosition.x - _initialPosition.x);
+        if (_distanceMoved < 0.4 * Screen.width)
+        {
+            transform.localPosition = _initialPosition;
+            transform.localEulerAngles = Vector3.zero;
+        }
+        else
+        {
+            if (transform.localPosition.x > _initialPosition.x)
+            {
+                _swipeLeft = false;
+
+            }
+            else
+            {
+                _swipeLeft = true;
+            }
+            StartCoroutine(MovedCard());
+        }
+    }
+
+    private IEnumerator MovedCard()
+    {
+        float time = 0;
+        while (GetComponent<Image>().color != new Color(1, 1, 1, 0))
+        {
+            time += Time.deltaTime;
+            if (_swipeLeft)
+            {
+                transform.localPosition = new Vector3(Mathf.SmoothStep(transform.localPosition.x,
+                    transform.localPosition.x - Screen.width, time), transform.localPosition.y, 0);
+            }
+            else
+            {
+                transform.localPosition = new Vector3(Mathf.SmoothStep(transform.localPosition.x,
+                    transform.localPosition.x + Screen.width, time), transform.localPosition.y, 0);
+            }
+            GetComponent<Image>().color = new Color(1, 1, 1, Mathf.SmoothStep(1, 0, 4 * time));
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+}
